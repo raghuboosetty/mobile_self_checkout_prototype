46144820ffdd24b7dac7e467aa1522f3cd61d6b2
Raghu Boosetty <raghu@rubyeffect.com>
2018-02-07 13:05:00 +0530

    change the barcode api request location and gradle build

--- app/src/main/java/com/reonios/mscoprototype/RestApi.java
+++ app/src/main/java/com/reonios/mscoprototype/RestApi.java
@@ -8,6 +8,6 @@ import retrofit2.http.Path;
  * Created by reonios on 5/10/16.
  */
 public interface RestApi {
-    @GET("samples/{id}")
+    @GET("locate/{id}/barcode")
     Call<Product> getProductDetails(@Path("id") String barcode);
 }
